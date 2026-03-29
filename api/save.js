a53be79a34d9a60420bab851d71eca40f4b97894
oprahvsiqpio16 <a77028488@gmail.com>
2026-03-29 21:53:17 +0300

    Create save.js

---
+++ api/save.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+
+    const { message } = req.body;
+    
+    const BOT_TOKEN = "8713127522:AAGfj4acg204MMc0SX7keJNtP4fWN9L-lYQ";
+    const CHAT_ID = "7984067238";
+
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+    try {
+        await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                chat_id: CHAT_ID,
+                text: message,
+                parse_mode: "Markdown" // هذا هو السر في ظهور الرسالة مرتبة
+            })
+        });
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
